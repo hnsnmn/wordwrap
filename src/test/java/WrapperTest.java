7fb4e08740c252228d0d6a24a29b1c8a0259d13c
{hongseongmin} <{hnsnmn@gmail.com}>
2014-01-14 15:18:11 +0900

    add git ignorefile

---
+++ src/test/java/WrapperTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 1. 14.
+ * Time: 오후 2:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class WrapperTest {
+	@Test
+	public void nothing() {
+
+	}
+}
